c38030f5a4f9580dfca5664940451fbe727c5b43
PurpleMagick <PurpleMagick@users.noreply.github.com>
2024-02-25 22:40:49 +0200

    Bumped version to 1.0.0

--- res/headers.js
+++ res/headers.js
@@ -3,7 +3,7 @@
 // @namespace		https://github.com/PurpleMagick/
 // @description		Network search pagination is broken and does not count pages correctly. This is a fix
 // @author			VLAZ
-// @version			0.0.1
+// @version			1.0.0
 //
 // @match           https://stackexchange.com/search*
 //
